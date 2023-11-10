adac29b507861aa1678406aba53f9acd16c35d0d
Rene Rendon <rene.rendon@hotmail.com>
2023-11-10 13:27:57 -0600

    End of section 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using DishesAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DishesDbContext>(o => o.UseSqlite(
+    builder.Configuration["ConnectionStrings:DishesDbConnectionString"]
+));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// recreate & migrate the database on each run, for demo purposes
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DishesDbContext>();
+    context.Database.EnsureDeleted();
+    context.Database.Migrate();
+}
+
+app.Run();
